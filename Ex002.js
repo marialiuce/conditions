ce9ce35843c763e250658d903b5c3467772ed0cd
Alice <malyefx@gmail.com>
2025-05-12 13:12:11 -0300

    first commit of repo

---
+++ Ex002.js
@@ -0,0 +1,9 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
+// O código acima verifica se o país é Brasil e imprime uma mensagem correspondente.
+// O operador de comparação utilizado é o "==".
